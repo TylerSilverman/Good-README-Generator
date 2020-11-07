70224e93927359cbf4c050caf36584d03f957dea
Tyler Silverman <silverman.tyler@gmail.com>
2020-11-07 10:53:29 -0500

    edited the order of the questions, and the choices

--- index.js
+++ index.js
@@ -4,6 +4,72 @@ const fs = require('fs');
 
 inquirer.prompt([
     {
+        //#1name of project
+        type: "input",
+        message:"What is the name of your project",
+        name: "title of project",
+    },
+    {
+        //#2decsription of project
+        type: "input",
+        message:"What is the description of your project",
+        name: "description of project",
+    },
+    {
+        // #3table of contents
+        type: "list",
+        message:"Table of Contents?",
+        name: "contents",
+        choices:["names", "examples", "questions", "feedback"],
+    },
+    {
+        //#4installation of project
+        type: "checkbox",
+        message:"What did you use to install the project?",
+        name: "installation",
+        choices: ["Didnt need anything", "javascript", "VS Code", "just installed all on its own"],
+    },
+    {
+        //#5usage for the project
+        type: "input",
+        message:"What usage did you use for the project?",
+        name: "usage used:",
+    },
+    {
+        //#6any liceneses used for project
+        type: "checkbox",
+        message:"What licenses did you use to create the project?",
+        name: "licenses used:",
+        choices:["Paython", "HTML/CSS", "Javascript", "Node"],
+    },
+    {
+        //#7any contributions used for project
+        type: "checkbox",
+        message:"Did you use any contributions to create the project?",
+        name: "contributed to the project:",
+        choices: ["Yes", "No"],
+    },
+    {
+        //#8testing procedures
+        type: "checkbox",
+        message:"What test procedures did you use for the project?",
+        name: "testing Procedures used:",
+        choices:["Procedure A", "Procedure B", "Procedure C", "Procedure D"],
+    },
+    {
+        //#9Questions
+        type: "checkbox",
+        message:"Do you have any questions?",
+        name: "questions",
+        choices: ["Yes", "No"]
+    },
+    {
+        //#9-1 Ask the question
+        type: "input",
+        message:"What is your question?",
+        name: "Question:",
+    },
+    {
         //github username
         type: "input",
         message:"What is your github username?",
@@ -16,74 +82,18 @@ inquirer.prompt([
         name: "email-Address",
     },
     {
-        //name of project
-        type: "input",
-        message:"What is the name of your project",
-        name: "title",
-    },
-    {
-        //decsription of project
-        type: "input",
-        message:"What is the description of your project",
-        name: "description",
-    },
-    {
-        //installation of project
-        type: "input",
-        message:"How did you install the project?",
-        name: "installation",
-    },
-    {
-        //testing procedures
-        type: "list",
-        message:"What test procedures did you use for the project?",
-        name: "testing",
-        choices:["Procedure A", "Procedure B", "Procedure C", "Procedure D"],
-    },
-    {
-        //any liceneses used for project
-        type: "checkbox",
-        message:"What licenses did you use to create the project?",
-        name: "license",
-        choices:["License A", "License B", "License C", "License D"],
-    },
-    {
-        //any liceneses used for project
-        type: "input",
-        message:"Did you use any contributions to create the project?",
-        name: "contribute",
-    },
-    {
-        //usage for the project
-        type: "input",
-        message:"Did you have any usage for the project?",
-        name: "usage",
-    },
-    {
-        //Contents and Questions
-        type: "input",
-        message:"Do you have any contents and or questions?",
-        name: "questions",
-    },
-    {
-        // table of contents
-        type: "list",
-        message:"Table of Contents?",
-        name: "contents",
-        choices:["names", "examples", "questions", "feedback"],
-    },
-    {
-        // password
+        // password is hidden
         type: "password",
         message:"What is your password?",
         name: "password",
     },
     {
-        // confirming the password
+        // confirming the password is hidden
         type: "password",
         message:"Re-enter the password to confirm?",
         name: "confirm",
     },
+    //function to create the information saved on another filename
 ]).then((data)=>{
     
 const filename = "log.txt";
