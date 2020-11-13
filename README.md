c5a9f903e3219b95d2712e2f923f9540057768f3
Tyler Silverman <silverman.tyler@gmail.com>
2020-11-12 19:50:41 -0500

    updated README

---
+++ README.md
@@ -0,0 +1,34 @@
+## Table of Contents: 
+## WHEN I click on the links in the Table of Contents
+## THEN I am taken to the corresponding section of the README
+
+* [Installation](#installation)
+To make sure that the README generator works, is download the repo from my github page below, also make sure to run: npm init and npm install inquirer to make sure its fully set up. 
+
+* [Usage](#usage)
+To use run the commande: node index.js and then will be prompted to begin. After completing the questions a generated README is created. 
+
+* [Contributing](#contribution)
+## WHEN I enter my project title
+## WHEN I enter my GitHub username, with a link to my GitHub profile
+## WHEN I enter my email address with instructions on how to reach me
+
+* [Tests](#test)
+
+To run adminstative tests, input the following command: `npm test tyler`
+
+* [License](#license)
+## WHEN I choose a license for my application from a list of options
+## THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+https://img.shields.io/badge/license-MIT-blue.svg
+https://img.shields.io/badge/license-APACHE-blue.svg
+https://img.shields.io/badge/license-github-blue.svg
+https://img.shields.io/badge/license-GPL-blue.svg
+https://img.shields.io/badge/license-BSD3-blue.svg
+https://img.shields.io/badge/license-None-blue.svg
+
+* [Questions](#questions)
+
+* If you have questions, please visit my [Github](https://github.com/TylerSilverman) 
+* Direct Contact $(adminInput.email)
+
--- index.js
+++ index.js
@@ -1,66 +1,17 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const savedInfo = require("savedInfo");
+
+
  
 
 inquirer.prompt([
-    {
-        //#1name of project
-        type: "input",
-        message:"What is the name of your project",
-        name: "title of project",
-    },
-    {
-        //#2decsription of project
-        type: "input",
-        message:"What is the description of your project",
-        name: "description of project",
-    },
     {
         // #3table of contents
         type: "list",
         message:"Table of Contents?",
         name: "contents",
-        choices:["names", "examples", "questions", "feedback"],
-    },
-    {
-        //#4installation of project
-        type: "checkbox",
-        message:"What did you use to install the project?",
-        name: "installation",
-        choices: ["Didnt need anything", "javascript", "VS Code", "just installed all on its own"],
-    },
-    {
-        //#5usage for the project
-        type: "input",
-        message:"What usage did you use for the project?",
-        name: "usage used:",
-    },
-    {
-        //#6any liceneses used for project
-        type: "checkbox",
-        message:"What licenses did you use to create the project?",
-        name: "licenses used:",
-        choices:["MIT", "APACHE 2.0", "GitHub", "GPL 3.0", "None"],
-    },
-    {
-        //#7any contributions used for project
-        type: "checkbox",
-        message:"Did you use any contributions to create the project?",
-        name: "contributed to the project:",
-        choices: ["Yes", "No"],
-    },
-    {
-        //#8 types of package names 
-        type: "checkbox",
-        message:"What type of package names did you use for the project?",
-        name: "Package Names used:",
-        choices:["APM", "Bower", "CRAN/METACRAN", "PyPI"],
-    },
-    {
-        //#9If you have Questions leave contact information
-        type: "input",
-        message:"If you have questions, please leave your contact information and I will reach out to you.",
-        name: "input",
+        choices:["Installation", "Contribution", "Test", "Usage", "Licenses", "Questions"],
     },
     {
         //github username
@@ -86,6 +37,58 @@ inquirer.prompt([
         message:"Re-enter the password to confirm?",
         name: "confirm",
     },
+    {
+        //#1name of project
+        type: "input",
+        message:"What is the name of your project",
+        name: "title of project",
+    },
+    {
+        //#2decsription of project
+        type: "input",
+        message:"What is the description of your project",
+        name: "description of project",
+    },
+    {
+        //#4installation of project
+        type: "checkbox",
+        message:"What did you use to install the project?",
+        name: "installation",
+        choices: ["Moment", "npm", "API", "package.JSON"],
+    },
+    {
+        //#5usage for the project
+        type: "input",
+        message:"What usage did you use for the project?",
+        name: "usage used:",
+    },
+    {
+        //#6any liceneses used for project
+        type: "checkbox",
+        message:"What licenses did you use to create the project?",
+        name: "licenses used:",
+        choices:["MIT", "APACHE 2.0", "GitHub", "GPL 3.0", "BSD 3", "None"],
+    },
+    {
+        //#7any contributions used for project
+        type: "checkbox",
+        message:"Did you use any contributions to create the project?",
+        name: "contributed to the project:",
+        choices: ["Yes", "No"],
+    },
+    {
+        //#8 types of package names 
+        type: "checkbox",
+        message:"What type of package names did you use for the project?",
+        name: "Package Names used:",
+        choices:["APM", "Bower", "CRAN/METACRAN", "PyPI"],
+    },
+    {
+        //#9If you have Questions leave contact information
+        type: "input",
+        message:"If you have questions, please leave your contact information and I will reach out to you.",
+        name: "input",
+    }
     //function to create the information saved on another filename
 ]).then((data)=>{
     
